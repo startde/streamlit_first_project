7c36c45751a25ad342119b23b7ff98232d2c7412
startde <rudash.ak@gmail.com>
2024-06-29 14:44:10 +0300

    Add files via upload

---
+++ main_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\SUBRU\Downloads\bike-sharing-demand\test.csv")
+
+st.title ("Bike Sharing Demand")
+st.subheader ("Raw Data")
+st.dataframe(df)
+
+weather = st.slider(
+    "Выбери погоду",
+    min_value=1,
+    max_value=4,
+    value=2,
+    step=None,
+)
+
+season = st.slider(
+    "Выбери время года",
+    min_value=1,
+    max_value=4,
+    value=2,
+    step=None,
+)
+
+working_day = st.slider(
+    "Выбери рабочий/нерабочий день",
+    min_value=0,
+    max_value=1,
+    value=0,
+    step=None,
+)
+
+holiday = st.slider(
+    "Выбери праздничный день",
+    min_value=0,
+    max_value=1,
+    value=0,
+    step=None,
+)
+
+st.subheader ("Modified Data")
+st.dataframe(df.loc[((df['weather'] == weather) & (df['season'] == season) & ((df['holiday'] == holiday) & ((df['workingday'] == working_day))))])
